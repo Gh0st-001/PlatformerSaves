de699679df5bd6d063706af2092fe88beffd4f0b
Gh0st-001 <armorfinal@gmail.com>
2026-05-11 11:31:03 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,19 @@
 #include <Geode/Geode.hpp>
 #include <geode.custom-keybinds/include/Keybinds.hpp>
 #include <sabe.persistenceapi/include/PersistenceAPI.hpp>
-
 using namespace geode::prelude;
 using namespace persistenceAPI;
-
 void setupKeybinds();
-
 $on_mod(Loaded) {
     setupKeybinds();
 }
-
 void setupKeybinds() {
     using namespace keybinds;
-
     BindManager::get()->registerBindable({
         "save-game"_spr,
         "Save game",
         "Hotkey for quick saving",
-        { Keybind::create(KEY_K, Modifier::None) },
+        { keybinds::Keybind::create(KEY_K, Modifier::None) },
         Category::PLAY,
     });
 }
